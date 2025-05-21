b71906d2ae81dd4a01da2dbb62fc1e83eecd47d9
ash1198 <ash.ranjan09@gmail.com>
2025-05-21 10:25:34 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Header, Main, Nav, Wrapper } from './App.styled'
+
+const App = () => {
+    return (
+        <Wrapper>
+            <Header>header</Header>
+            <Nav>nav</Nav>
+            <Main>main</Main>
+            <Footer>footer</Footer>
+        </Wrapper>
+    )
+}
+
+export default App
---
+++ src/App.styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    position: relative;
+    height: 100vh;
+`;
+
+export const Header = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 40px;
+    background-color: #000;
+    color: #fff;
+`;
+
+export const Footer = styled.footer`
+    height: 30px;
+`;
+
+export const Nav = styled.nav`
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 100vh;
+    z-index: 9999;
+`;
+
+export const Main = styled.main``;
